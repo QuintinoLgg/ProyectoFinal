b2d673ea2d4aca7f92a19867210cd25f9c60fa7a
quintinolgg <s20120151@alumnos.itsur.edu.mx>
2023-10-09 16:35:44 -0600

    Arreglo en la BottomBar

--- ProyectFinal/app/src/main/java/com/example/proyectfinal/navigation/AppScreens.kt
+++
@@ -1,6 +0,0 @@
-package com.example.proyectfinal.navigation
-
-sealed class AppScreens (val route: String){
-    object HomeScreen: AppScreens("home_screen")
-    object AddEditNoteScreen: AppScreens("add_edit_note_screen")
-}
