db72b5df35c3642763c0decff2557a0de97bc95b
quintinolgg <s20120151@alumnos.itsur.edu.mx>
2023-10-31 20:58:48 -0600

    viewModel

---
+++ ProyectFinal/app/src/main/java/com/example/proyectfinal/ui/theme/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.proyectfinal.ui.theme
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel() : ViewModel() {
+    var date: String  = ""
+    //Ventana AddEditNote parte
+    var tittle: String  = ""
+    var subject: String  = ""
+    var dateSubject: String  = ""
+    var description: String  = ""
+}
+
